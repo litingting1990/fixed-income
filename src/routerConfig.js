cbb42939550b1c4986a932b3c7c275a30b99a596
zhangliu <zhangliu@migu.cn>
2019-06-03 17:29:34 +0800

    50-character subject line
    Code Source From: Self Code
    
    Description:
    Jira:
    市场项目编号（名称）：

--- src/routerConfig.js
+++ src/routerConfig.js
@@ -13,72 +13,15 @@ const SalesRepository = React.lazy(() => import('./pages/riskManagement/SalesRep
 
 const UserLogin = React.lazy(() => import('./pages/UserLogin'));
 const UserRegister = React.lazy(() => import('./pages/UserRegister'));
-const Dashboard = React.lazy(() => import('./pages/Dashboard'));
-const Charts = React.lazy(() => import('./pages/Charts'));
-const BasicCharts = React.lazy(() => import('./pages/BasicCharts'));
-const Terms = React.lazy(() => import('./pages/Terms'));
-const Result = React.lazy(() => import('./pages/Result'));
-const BasicList = React.lazy(() => import('./pages/BasicList'));
-const ProjectList = React.lazy(() => import('./pages/ProjectList'));
-const BasicTable = React.lazy(() => import('./pages/BasicTable'));
-const GeneralTable = React.lazy(() => import('./pages/GeneralTable'));
-const Profile = React.lazy(() => import('./pages/Profile'));
-const Setting = React.lazy(() => import('./pages/Setting'));
-const Fail = React.lazy(() => import('./pages/Fail'));
+
 const Empty = React.lazy(() => import('./pages/Exception/Empty'));
 const Forbidden = React.lazy(() => import('./pages/Exception/Forbidden'));
 const NotFound = React.lazy(() => import('./pages/Exception/NotFound'));
 const ServerError = React.lazy(() => import('./pages/Exception/ServerError'));
 
 const routerConfig = [
-  {
-    path: '/dashboard/monitor',
-    component: Dashboard
-  },
-  {
-    path: '/chart/general',
-    component: Charts
-  },
-  {
-    path: '/chart/basic',
-    component: BasicCharts
-  },
-  {
-    path: '/list/basic',
-    component: BasicList
-  },
-  {
-    path: '/list/general',
-    component: ProjectList
-  },
-  {
-    path: '/result/success',
-    component: Result
-  },
-  {
-    path: '/result/fail',
-    component: Fail
-  },
-  {
-    path: '/table/basic',
-    component: BasicTable
-  },
-  {
-    path: '/profile/basic',
-    component: Profile
-  },
-  {
-    path: '/profile/general',
-    component: Terms
-  },
-  {
-    path: '/table/general',
-    component: GeneralTable
-  },
-  {
-    path: '/account/setting',
-    component: Setting
-  },
+
+
   {
     path: '/exception/500',
     component: ServerError
@@ -95,10 +38,7 @@ const routerConfig = [
     path: '/exception/404',
     component: NotFound
   },
-  {
-    path: '/user/login',
-    component: UserLogin
-  },
+
   {
     path: '/user/register',
     component: UserRegister
@@ -107,10 +47,7 @@ const routerConfig = [
     path: '/user/login',
     component: UserLogin
   },
-  {
-    path: '/user/register',
-    component: UserRegister
-  },
+
   {
     path: '/riskManagement/antiMoney',
     component: AntiMoney
