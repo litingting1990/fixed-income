8fd9ed711f90997dd0f137ac9253e690ae86ab93
zhangliu <zhangliu@migu.cn>
2019-06-03 14:14:38 +0800

    50-character subject line
    Code Source From: Self Code
    
    Description:
    Jira:
    市场项目编号（名称）：

--- src/menuConfig.js
+++ src/menuConfig.js
@@ -3,45 +3,19 @@
 // asideMenuConfig：侧边导航配置
 
 const headerMenuConfig = [
-  {
-    name: '首页',
-    path: '/',
-    icon: 'home',
-  },
-  {
-    name: '反馈',
-    path: 'https://github.com/alibaba/ice',
-    external: true,
-    newWindow: true,
-    icon: 'message',
-  },
-  {
-    name: '帮助',
-    path: 'https://alibaba.github.io/ice',
-    external: true,
-    newWindow: true,
-    icon: 'bangzhu',
-  },
+
 ];
 
 const asideMenuConfig = [
   {
-    name: '管理概况',
-    path: '/manage',
-    icon: 'account',
+    name: '风险控制',
+    path: '/riskManagement',
+    icon: 'shezhi',
     children: [
-      { name: '公司管理', path: '/manage/company', icon: 'set' },
-      { name: '部门管理', path: '/manage/department', icon: 'lights' },
-      { name: '团队管理', path: '/manage/team', icon: 'atm' },
-    ],
-  },
-  {
-    name: '专项管理',
-    path: '/special',
-    icon: 'set',
-    children: [
-      { name: '成本管理', path: '/special/cost', icon: 'refresh' },
-      { name: '质量管理', path: '/special/cluster', icon: 'account' },
+      { name: '销售对手库管理', path: '/riskManagement/salesRepository' },
+      { name: '信用风险管理', path: '/riskManagement/creditRisk' },
+      { name: '反洗钱管理', path: '/riskManagement/antiMoney' },
+      { name: '关联机构名单', path: '/riskManagement/relevance' },
     ],
   },
 ];
